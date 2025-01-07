7b97f56ff9d95dc8f255be87ba2e048feea74aa6
samhatter <167645840+samhatter@users.noreply.github.com>
2025-01-07 18:00:16 +0000

    ok, it actually works now

--- src/post_up.sh
+++ src/post_up.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-LINE_NUMBER=0
+LINE_NUMBER=1
 
 while true; do
-    SLSKD_SLSK_USERNAME=$(sed "${LINE_NUMBER}q;d" "$USERNAME_FILE")
+    SLSKD_SLSK_USERNAME=$(sed "${LINE_NUMBER}q;d" "/scripts/usernames.txt")
     if [ -n "$SLSKD_SLSK_USERNAME" ]; then
         export SLSKD_SLSK_USERNAME="$SLSKD_SLSK_USERNAME"
         echo "Logging into $SLSKD_SLSK_USERNAME"
         cd /slskd
         ./slskd & slskd_pid=$!
-        sleep 60
+        sleep 10
         kill $slskd_pid
         sleep $INTERVAL
         LINE_NUMBER=$((LINE_NUMBER + 1))
